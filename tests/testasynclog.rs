8ce5c3f44a94ec68c0773dd303a7919d2ce2e833
donnie4w <donnie4w@gmail.com>
2024-08-07 09:10:47 +0800

    Update testasynclog.rs

--- tests/testasynclog.rs
+++ tests/testasynclog.rs
@@ -1,16 +1,14 @@
 use std::{borrow::BorrowMut, sync::Arc};
 
 use tklog::{
-    async_debug, async_debugs, async_error, async_errors, async_fatal, async_fatals, async_info,
-    async_infos, async_trace, async_traces, async_warn, async_warns, Format, ASYNC_LOG, LEVEL,
-    MODE,
+    async_debug, async_debugs, async_error, async_errors, async_fatal, async_fatals, async_info, async_infos, async_trace, async_traces, async_warn, async_warns, Format, LogContext, ASYNC_LOG, LEVEL, MODE
 };
 use tokio::{sync::Mutex, time::Instant};
 
 async fn async_log_init() {
     ASYNC_LOG
         .set_console(true)
-        .set_level(LEVEL::Trace)
+        .set_level(LEVEL::Error)
         .set_format(Format::LevelFlag | Format::Date | Format::Time | Format::ShortFileName)
         .set_cutmode_by_size("tklog_async.log", 10000, 10, true)
         .await;
@@ -95,3 +93,53 @@ async fn testlogsize() {
 
     tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
 }
+
+
+#[tokio::test]
+async fn test_custom() {
+    fn custom_handle(lc: &LogContext) -> bool {
+        println!("level >>>>>>>>>>>>>>>>>{:?}", lc.level);
+        println!("message >>>>>>>>>>>>>>>>>{:?}", lc.log_body);
+        println!("filename >>>>>>>>>>>>>>>>>{:?}", lc.filename);
+        println!("line >>>>>>>>>>>>>>>>>{:?}", lc.line);
+        println!("modname >>>>>>>>>>>>>>>>>{:?}", lc.modname);
+        if lc.level == LEVEL::Debug {
+            println!("{}", "debug now");
+            return false;
+        }
+        true
+    }
+    ASYNC_LOG.set_custom_handler(custom_handle);
+    async_debug!("000000000000000000");
+    async_info!("1111111111111111111");
+    tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
+}
+
+#[tokio::test]
+async fn test_custom_multi() {
+    fn custom_handle(lc: &LogContext) -> bool {
+        println!("level >>>>>>>>>>>>>>>>>{:?}", lc.level);
+        println!("message >>>>>>>>>>>>>>>>>{:?}", lc.log_body);
+        println!("filename >>>>>>>>>>>>>>>>>{:?}", lc.filename);
+        println!("line >>>>>>>>>>>>>>>>>{:?}", lc.line);
+        println!("modname >>>>>>>>>>>>>>>>>{:?}", lc.modname);
+        if lc.level == LEVEL::Debug {
+            println!("{}", "debug now");
+            return false;
+        }
+        true
+    }
+    let mut log = tklog::Async::Logger::new();
+    log.set_custom_handler(custom_handle);
+    let mut logger = Arc::clone(&Arc::new(Mutex::new(log)));
+    let log = logger.borrow_mut();
+    async_traces!(log, "async_traces>>>>", "AAAAAAAAAA", 1, 2, 3);
+    async_debugs!(log, "async_debugs>>>>", "BBBBBBBBBB", 1, 2, 3);
+    async_infos!(log, "async_infos>>>>", "CCCCCCCCCC", 1, 2, 3);
+    async_warns!(log, "async_warns>>>>", "DDDDDDDDDD", 1, 2, 3);
+    async_errors!(log, "async_errors>>>>", "EEEEEEEEEEE", 1, 2, 3);
+    async_fatals!(log, "async_fatals>>>>", "FFFFFFFFFFFF", 1, 2, 3);
+
+    tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
+}
+
