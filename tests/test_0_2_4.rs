9e0ce04de421b582c2b3ed49425b6fada8dfed36
donnie4w <donnie4w@gmail.com>
2024-10-21 20:51:42 +0800

    Create test_0_2_4.rs

---
+++ tests/test_0_2_4.rs
@@ -0,0 +1,81 @@
+use std::{thread, time::Duration};
+use tklog::{async_debug, async_error, async_fatal, async_info, async_trace, async_warn, ASYNC_LOG, LOG};
+use tklog::{debug, error, fatal, info, trace, warn, LEVEL};
+
+#[test]
+fn testlog() {
+    LOG.set_level(LEVEL::Trace).set_attr_format(|fmt| {
+        fmt.set_body_fmt(|level, body| {
+            //处理body的末尾换行符
+            let trimmed_body = if body.ends_with('\n') {
+                format!("{}{}", body.as_str()[..body.len() - 1].to_string(), "\x1b[0m\n")
+            } else {
+                format!("{}{}", body, "\x1b[0m\n")
+            };
+
+            match level {
+                LEVEL::Trace => format!("{}{}", "\x1b[34m", trimmed_body), //蓝色
+                LEVEL::Debug => format!("{}{}", "\x1b[36m", trimmed_body), //青色
+                LEVEL::Info => format!("{}{}", "\x1b[32m", trimmed_body),  //绿色
+                LEVEL::Warn => format!("{}{}", "\x1b[33m", trimmed_body),  //黄色
+                LEVEL::Error => format!("{}{}", "\x1b[31m", trimmed_body), //红色
+                LEVEL::Fatal => format!("{}{}", "\x1b[41m", trimmed_body), //背景红
+                LEVEL::Off => "".to_string(),
+            }
+        });
+    });
+
+    trace!("trace!", "this is sync log");
+    debug!("debug!", "this is sync log");
+    info!("info!", "this is sync log");
+    warn!("warn!", "this is sync log");
+    error!("error!", "this is sync log");
+    fatal!("fata!", "this is sync log");
+    thread::sleep(Duration::from_secs(1))
+}
+
+#[tokio::test]
+async fn asynctestlog() {
+    ASYNC_LOG.set_level(LEVEL::Trace).set_attr_format(|fmt| {
+        fmt.set_body_fmt(|level, body| {
+            
+            //处理body的末尾换行符
+            let trimmed_body = if body.ends_with('\n') {
+                format!("{}{}", body.as_str()[..body.len() - 1].to_string(), "\x1b[0m\n")
+            } else {
+                format!("{}{}", body, "\x1b[0m\n")
+            };
+
+            match level {
+                LEVEL::Trace => format!("{}{}", "\x1b[34m", trimmed_body), //蓝色
+                LEVEL::Debug => format!("{}{}", "\x1b[36m", trimmed_body), //青色
+                LEVEL::Info => format!("{}{}", "\x1b[32m", trimmed_body),  //绿色
+                LEVEL::Warn => format!("{}{}", "\x1b[33m", trimmed_body),  //黄色
+                LEVEL::Error => format!("{}{}", "\x1b[31m", trimmed_body), //红色
+                LEVEL::Fatal => format!("{}{}", "\x1b[41m", trimmed_body), //背景红
+                LEVEL::Off => "".to_string(),
+            }
+        });
+        fmt.set_level_fmt(|level| {
+            match level {
+                LEVEL::Trace => "[AT]",
+                LEVEL::Debug => "[AD]",
+                LEVEL::Info => "[AI]",
+                LEVEL::Warn => "[AW]",
+                LEVEL::Error => "[AE]",
+                LEVEL::Fatal => "[AF]",
+                LEVEL::Off => "",
+            }
+            .to_string()
+        });
+    });
+
+    async_trace!("trace!", "this is async log");
+    async_debug!("debug!", "this is async log");
+    async_info!("info!", "this is async log");
+    async_warn!("warn!", "this is async log");
+    async_error!("error!", "this is async log");
+    async_fatal!("fata!", "this is async log");
+
+    tokio::time::sleep(tokio::time::Duration::from_secs(3)).await;
+}
